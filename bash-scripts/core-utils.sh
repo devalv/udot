77e6c9d6fa7cf65c0182f278da603e887db42f93
Aleksei Devyatkin <aleksei@devyatkin.dev>
2022-07-31 11:39:57 +0300

    fixes pt.4

--- bash-scripts/core-utils.sh
+++ bash-scripts/core-utils.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-apt install snapd zsh exa curl wget tmux top bpytop bat gnupg \
+sudo apt-get install snapd zsh exa curl wget tmux top bpytop bat gnupg \
 jsonlint yamllint fzf nmap pwgen vim mc telnet p7zip-full rar \
 unrar terminator gnome-tweaks vlc openvpn openssh-client \
-default-jre fonts-firacode thefuck
+default-jre fonts-firacode thefuck -y
 
 exit 0
