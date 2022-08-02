b4e0cee66eebc102009c20ab3e589570162192c3
Aleksei <63240599+devalv@users.noreply.github.com>
2022-08-03 01:27:21 +0300

    Update go-stuff.sh

--- bash-scripts/go-stuff.sh
+++ bash-scripts/go-stuff.sh
@@ -3,8 +3,7 @@
 set -e
 
 echo "installing golang"
-#TODO: install from https://go.dev/dl/
-#sudo apt-get install golang -y
+sudo apt-get install golang -y
 
 echo "installing goimports"
 go install golang.org/x/tools/cmd/goimports@latest
