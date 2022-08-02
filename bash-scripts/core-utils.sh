699694339fa740439b8685f1c5e4b916a6aae855
Aleksei <63240599+devalv@users.noreply.github.com>
2022-08-03 01:00:17 +0300

    Update core-utils.sh

--- bash-scripts/core-utils.sh
+++ bash-scripts/core-utils.sh
@@ -5,7 +5,6 @@ set -e
 echo "Installing core system utils"
 apt-get install zsh exa curl wget tmux bpytop bat gnupg \
 jsonlint yamllint fzf pwgen vim mc telnet p7zip-full rar \
-unrar terminator vlc openvpn openssh-client \
-default-jre fonts-firacode -y
+unrar terminator vlc openssh-client default-jre fonts-firacode -y
 
 exit 0
