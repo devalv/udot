92bc70b9c9a2b82eda15094748681b68a6aec0fc
Aleksei Devyatkin <aleksei@devyatkin.dev>
2022-10-18 18:30:16 +0300

    WIP: actualization

--- bash-scripts/oh-my-zsh.sh
+++ bash-scripts/oh-my-zsh.sh
@@ -2,8 +2,6 @@
 
 set -e
 
-rm -rf ~/.oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-mv ~/.zshrc.pre-oh-my-zsh ~/.zshrc
 
 exit 0
--- bash-scripts/python-stuff.sh
+++ bash-scripts/python-stuff.sh
@@ -10,11 +10,12 @@ sudo apt-get install -y build-essential libssl-dev zlib1g-dev libbz2-dev \
 libreadline-dev libsqlite3-dev llvm libncurses5-dev libncursesw5-dev \
 xz-utils tk-dev libffi-dev liblzma-dev make make gcc curl
 
-#echo "removing existing pyenv installation"
-#rm -rf /home/$USER/.pyenv
+echo "installing pyenv"
+curl https://pyenv.run | bash
 
-#echo "installing pyenv"
-#curl https://pyenv.run | bash
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
+echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
+echo 'eval "$(pyenv init -)"' >> ~/.zshrc
 
 #echo "installing python 3.10.5 via pyenv"
 #/home/$USER/.pyenv/bin/pyenv install 3.10.5 -s
@@ -29,7 +30,7 @@ xz-utils tk-dev libffi-dev liblzma-dev make make gcc curl
 #mkdir $ZSH_CUSTOM/plugins/poetry
 #~/.poetry/bin/poetry completions zsh > $ZSH_CUSTOM/plugins/poetry/_poetry
 
-echo "install my zshrc back"
-mv ~/.zshrc.pre-oh-my-zsh ~/.zshrc
+#echo "install my zshrc back"
+#mv ~/.zshrc.pre-oh-my-zsh ~/.zshrc
 
 exit 0
