139f4f1dae4155ff28b8ec9f19a5eea3e521f85c
Aleksei <63240599+devalv@users.noreply.github.com>
2023-04-28 12:50:28 +0300

    Update js-stuff.sh

--- bash-scripts/js-stuff.sh
+++ bash-scripts/js-stuff.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-echo "Installing Nodejs 16 LTS"
+echo "Installing Nodejs 18 LTS"
 curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
 sudo apt-get install -y nodejs
 node -v
